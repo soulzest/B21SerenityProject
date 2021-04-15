b229964a676678222556482b9a396ad540d5a1a8
Akbar <akbar@cybertekschool.com>
2021-04-15 15:39:37 -0400

    Editor Serenity Post Test starter

---
+++ src/test/java/b21/spartan/editor/SpartanEditorRolePostTest.java
@@ -0,0 +1,35 @@
+package b21.spartan.editor;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.*;
+import static net.serenitybdd.rest.SerenityRest.*;
+import static org.hamcrest.Matchers.*;
+
+@SerenityTest
+public class SpartanEditorRolePostTest {
+
+
+    @BeforeAll
+    public static void init(){
+
+        RestAssured.baseURI = "http://18.235.32.166:7000" ;
+        RestAssured.basePath = "/api" ;
+
+    }
+    @AfterAll
+    public static void cleanup(){
+        reset();
+    }
+
+    @DisplayName("Editor Should be able Post Valid Data")
+    @Test
+    public void testEditorPostData(){
+
+
+    }
+
+
+}
