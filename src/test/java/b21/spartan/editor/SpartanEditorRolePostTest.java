38601511b0a9f335e15fdd1e48f61601dc2bee58
Akbar <akbar@cybertekschool.com>
2021-04-16 10:18:16 -0400

    Serenity Day 2 : Parameterized Test from CSV source for editor post

--- src/test/java/b21/spartan/editor/SpartanEditorRolePostTest.java
+++ src/test/java/b21/spartan/editor/SpartanEditorRolePostTest.java
@@ -91,6 +91,36 @@ public class SpartanEditorRolePostTest {
 
         System.out.println("bodyMap = " + bodyMap);
 
+        SerenityRest.given()
+                .auth().basic("editor","editor")
+                .log().body()
+                .contentType(ContentType.JSON)
+                .body(bodyMap).
+                when()
+                .post("/spartans").prettyPeek() ;
+        // Do all assertions here
+        Ensure.that("It ran successfully", thenSection-> thenSection.statusCode( equalTo(201)  ) );
+        Ensure.that("Response format is correct" , thenSection -> thenSection.contentType(ContentType.JSON) );
+
+        Ensure.that("success message is correct" , v -> v.body("success", is("A Spartan is Born!")   )       ) ;
+        Ensure.that("ID is generated and not null" , v-> v.body("data.id" , notNullValue()      ) ) ;
+        // checking actual data
+        Ensure.that("name is correct" ,
+                v-> v.body("data.name" ,  is(bodyMap.get("name")) )
+        ) ;
+        Ensure.that("gender is correct" ,
+                v-> v.body("data.gender" ,  is(bodyMap.get("gender")) )
+        ) ;
+        Ensure.that("phone is correct" ,
+                v-> v.body("data.phone" ,  is(bodyMap.get("phone")) )
+        ) ;
+        // check Location header end with newly generated id
+        String newId = lastResponse().path("data.id").toString() ;
+        System.out.println(   lastResponse().header("Location")   );
+
+        Ensure.that("location header end with "+ newId ,
+                v-> v.header("Location" , endsWith(newId)  )
+        )  ;
 
 
     }
